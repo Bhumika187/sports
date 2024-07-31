45e85c7842204a962f93df90bbe48ce3cdffb30c
Bhumika187 <pantbhumika5@gmail.com>
2024-08-01 01:22:36 +0530

    new

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -18,30 +18,29 @@ const Card = () => {
   };
 
   return (
-    <div className={`min-h-screen flex flex-col justify-center items-center transition-colors duration-500 ${theme === "darkMode" ? 'bg-black text-white' : 'bg-white text-black'}`}>
+    <div className={`min-h-screen flex flex-col justify-center items-center transition-colors duration-500 ${theme === "darkMode" ? 'bg-gradient-to-br from-white via-[#18282A] to-[#221A2C] text-white' : 'bg-white text-black'}`}>
       <button
         onClick={handleThemeSwitch}
         className={`absolute top-10 right-10 px-4 py-2 rounded ${theme === "darkMode" ? 'bg-white text-black' : 'bg-black text-white'}`}
       >
         {theme === "darkMode" ? 'Light' : 'Dark'} Mode
       </button>
-      <div className="flex flex-wrap justify-center gap-1">
+      <div className="flex flex-wrap justify-center gap-4">
         {data.slice(0, 4).map((product) => {
           const { image, title, totalEvents, sport } = product;
           return (
             <section
-             
-              className={`card flex flex-col items-center hover:cursor-pointer hover:scale-110 duration-300 mx-2 my-4 bg-white w-90 h-54 transition duration-500 ${theme === "darkMode" ? 'bg-[#3B3E47] text-white' : ''}`}
               key={title}
+              className={`card flex flex-col items-center hover:cursor-pointer hover:scale-110 duration-300 mx-2 my-4 w-90 h-54 transition duration-500 ${theme === "darkMode" ? 'bg-[#3B3E47] text-white' : 'bg-white text-black'}`}
             >
               <img src={image} alt="hero-img" className="card-img" />
               <div className="card-title font-semibold">{title}</div>
-              <div className={`flex flex-wrap bg-gray-200 text-black ${theme === "darkMode" ? 'bg-[#292B32] text-white' : ''}`}>
-                <div className="flex flex-col bg-gray-200 mr-10 text-black">
+              <div className={`flex flex-wrap ${theme === "darkMode" ? 'bg-[#292B32] text-white' : 'bg-gray-200 text-black'}`}>
+                <div className="flex flex-col mr-10">
                   <div>Total Events</div>
                   <div className="card-event font-semibold">{totalEvents}</div>
                 </div>
-                <div className="flex flex-col bg-gray-200 text-black">
+                <div className="flex flex-col">
                   <div>Sport</div>
                   <div className="card-sport font-semibold">{sport}</div>
                 </div>
